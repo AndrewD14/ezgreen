b3123a2bd161249fb48ce850b84685e7ff26d80d
ajd2144 <andrew.damico@northwestern.edu>
2023-03-11 19:58:11 -0600

    Adjust the names of the sensors in the test data.

--- database/test_seeds/test_data.sql
+++ database/test_seeds/test_data.sql
@@ -5,19 +5,19 @@ TRUNCATE TABLE public.sensor;
 INSERT INTO public.sensor
 (id,type,port,board,low_calibration,high_calibration,created_by,updated_by,created_ts,updated_ts)
 VALUES
-(1,'water level',1,1,20.2,1.8,'a.damico','a.damico',CURRENT_TIMESTAMP,CURRENT_TIMESTAMP),
-(2,'humidity/temperature',1,2,NULL,NULL,'a.damico','a.damico',CURRENT_TIMESTAMP,CURRENT_TIMESTAMP),
-(3,'humidity/temperature',1,3,NULL,NULL,'a.damico','a.damico',CURRENT_TIMESTAMP,CURRENT_TIMESTAMP),
-(4,'humidity/temperature',1,4,NULL,NULL,'a.damico','a.damico',CURRENT_TIMESTAMP,CURRENT_TIMESTAMP),
-(5,'humidity/temperature',1,5,NULL,NULL,'a.damico','a.damico',CURRENT_TIMESTAMP,CURRENT_TIMESTAMP),
-(6,'luminosity',1,6,NULL,NULL,'a.damico','a.damico',CURRENT_TIMESTAMP,CURRENT_TIMESTAMP),
-(7,'luminosity',1,7,NULL,NULL,'a.damico','a.damico',CURRENT_TIMESTAMP,CURRENT_TIMESTAMP),
-(8,'luminosity',1,8,NULL,NULL,'a.damico','a.damico',CURRENT_TIMESTAMP,CURRENT_TIMESTAMP),
-(9,'plant moister',2,1,1.4,3.8,'a.damico','a.damico',CURRENT_TIMESTAMP,CURRENT_TIMESTAMP),
-(10,'plant moister',2,3,1.44,3.9,'a.damico','a.damico',CURRENT_TIMESTAMP,CURRENT_TIMESTAMP),
-(11,'plant moister',2,5,1.38,3.78,'a.damico','a.damico',CURRENT_TIMESTAMP,CURRENT_TIMESTAMP),
-(12,'plant moister',2,7,1.42,3.8,'a.damico','a.damico',CURRENT_TIMESTAMP,CURRENT_TIMESTAMP),
-(13,'plant moister',3,1,1.39,3.82,'a.damico','a.damico',CURRENT_TIMESTAMP,CURRENT_TIMESTAMP);
+(1,'Water level',1,1,20.2,1.8,'a.damico','a.damico',CURRENT_TIMESTAMP,CURRENT_TIMESTAMP),
+(2,'Humidity/Temperature',1,2,NULL,NULL,'a.damico','a.damico',CURRENT_TIMESTAMP,CURRENT_TIMESTAMP),
+(3,'Humidity/Temperature',1,3,NULL,NULL,'a.damico','a.damico',CURRENT_TIMESTAMP,CURRENT_TIMESTAMP),
+(4,'Humidity/Temperature',1,4,NULL,NULL,'a.damico','a.damico',CURRENT_TIMESTAMP,CURRENT_TIMESTAMP),
+(5,'Humidity/Temperature',1,5,NULL,NULL,'a.damico','a.damico',CURRENT_TIMESTAMP,CURRENT_TIMESTAMP),
+(6,'Luminosity',1,6,NULL,NULL,'a.damico','a.damico',CURRENT_TIMESTAMP,CURRENT_TIMESTAMP),
+(7,'Luminosity',1,7,NULL,NULL,'a.damico','a.damico',CURRENT_TIMESTAMP,CURRENT_TIMESTAMP),
+(8,'Luminosity',1,8,NULL,NULL,'a.damico','a.damico',CURRENT_TIMESTAMP,CURRENT_TIMESTAMP),
+(9,'Plant moisture indicator',2,1,1.4,3.8,'a.damico','a.damico',CURRENT_TIMESTAMP,CURRENT_TIMESTAMP),
+(10,'Plant moisture indicator',2,3,1.44,3.9,'a.damico','a.damico',CURRENT_TIMESTAMP,CURRENT_TIMESTAMP),
+(11,'Plant moisture indicator',2,5,1.38,3.78,'a.damico','a.damico',CURRENT_TIMESTAMP,CURRENT_TIMESTAMP),
+(12,'Plant moisture indicator',2,7,1.42,3.8,'a.damico','a.damico',CURRENT_TIMESTAMP,CURRENT_TIMESTAMP),
+(13,'Plant moisture indicator',3,1,1.39,3.82,'a.damico','a.damico',CURRENT_TIMESTAMP,CURRENT_TIMESTAMP);
 
 INSERT INTO public.environment
 (location,sensor_id,created_by,updated_by,created_ts,updated_ts)
