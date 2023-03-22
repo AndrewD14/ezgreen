90a720fa4fead9c8640ffb8f10163f52a220f4d7
Michael Damico <mdamico@hawk.iit.edu>
2023-03-21 23:52:08 -0500

    Corrected the file name by deleting and then readding.

---
+++ src/main/ui/src/service/utils/plantFormat.ts
@@ -0,0 +1,44 @@
+import _ from 'lodash';
+
+export function formatAll(data: any)
+{
+   let plants: any = [];
+
+   plants = data.plants.map((plant: any) => {
+      let cursor = 0;
+
+      if(plant.sensorId >= 1)
+      {
+         do
+         {
+            if(data.sensors[cursor].id === plant.sensorId) plant.sensor = structuredClone(data.sensors[cursor]);
+
+            cursor = cursor + 1;
+         }while(cursor < data.sensors.length && _.isEmpty(plant.sensor));
+      }
+
+      cursor = 0;
+
+      do
+      {
+         if(data.potSizes[cursor].id === plant.potSizeId) plant.potSize = structuredClone(data.potSizes[cursor]);
+
+         cursor = cursor + 1;
+      }while(cursor < data.potSizes.length && _.isEmpty(plant.potSize));
+
+      return plant;
+   });
+
+   return plants;
+}
+
+export function formatOne(data: any)
+{
+   let plant = data.plant;
+
+   if(!_.isEmpty(data.sensor)) plant.sensor = structuredClone(data.sensor);
+   
+   plant.potSize = structuredClone(data.potSize);
+
+   return plant;
+}
